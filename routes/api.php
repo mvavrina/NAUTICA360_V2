842f272552867b46f155ad0113748cc6627c0d98
JanSztefek <sztefek.jan@seznam.cz>
2025-02-14 14:12:49 +0100

    edit routes links

--- routes/api.php
+++ routes/api.php
@@ -1,80 +1,80 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Http;
+use App\Http\Controllers\ApiController;
 
 // Countries
-Route::get('/countries', 'ApiController@getCountries');
-Route::get('/country/{id}', 'ApiController@getCountry');
+Route::get('/countries', [ApiController::class, 'getCountries']);
+Route::get('/country/{id}', [ApiController::class, 'getCountry']);
 
 // World Regions
-Route::get('/worldRegions', 'ApiController@getWorldRegions');
-Route::get('/worldRegion/{id}', 'ApiController@getWorldRegion');
+Route::get('/worldRegions', [ApiController::class, 'getWorldRegions']);
+Route::get('/worldRegion/{id}', [ApiController::class, 'getWorldRegion']);
 
 // Sailing Areas
-Route::get('/sailingAreas', 'ApiController@getSailingAreas');
-Route::get('/sailingArea/{id}', 'ApiController@getSailingArea');
+Route::get('/sailingAreas', [ApiController::class, 'getSailingAreas']);
+Route::get('/sailingArea/{id}', [ApiController::class, 'getSailingArea']);
 
 // Bases
-Route::get('/bases', 'ApiController@getBases');
-Route::get('/base/{id}', 'ApiController@getBase');
+Route::get('/bases', [ApiController::class, 'getBases']);
+Route::get('/base/{id}', [ApiController::class, 'getBase']);
 
 // Equipment
-Route::get('/equipment', 'ApiController@getEquipment');
+Route::get('/equipment', [ApiController::class, 'getEquipment']);
 
 // Companies
-Route::get('/companies', 'ApiController@getCompanies');
-Route::get('/company/{id}', 'ApiController@getCompany');
+Route::get('/companies', [ApiController::class, 'getCompanies']);
+Route::get('/company/{id}', [ApiController::class, 'getCompany']);
 
 // Shipyards
-Route::get('/shipyards', 'ApiController@getShipyards');
-Route::get('/shipyard/{id}', 'ApiController@getShipyard');
+Route::get('/shipyards', [ApiController::class, 'getShipyards']);
+Route::get('/shipyard/{id}', [ApiController::class, 'getShipyard']);
 
 // Yachts
-Route::get('/yachts', 'ApiController@getYachts');
-Route::get('/yacht/{id}', 'ApiController@getYacht');
+Route::get('/yachts', [ApiController::class, 'getYachts']);
+Route::get('/yacht/{id}', [ApiController::class, 'getYacht']);
 
 // Yacht Types
-Route::get('/yachtTypes', 'ApiController@getYachtTypes');
+Route::get('/yachtTypes', [ApiController::class, 'getYachtTypes']);
 
 // Offers
-Route::get('/offers', 'ApiController@getOffers');
-Route::get('/specialOffers', 'ApiController@getSpecialOffers');
-Route::get('/specialOffers/{offerType}', 'ApiController@getSpecialOffersByType');
+Route::get('/offers', [ApiController::class, 'getOffers']);
+Route::get('/specialOffers', [ApiController::class, 'getSpecialOffers']);
+Route::get('/specialOffers/{offerType}', [ApiController::class, 'getSpecialOffersByType']);
 
 // Prices
-Route::get('/prices', 'ApiController@getPrices');
-Route::put('/setWeeklyPrice/{id}', 'ApiController@setWeeklyPrice');
+Route::get('/prices', [ApiController::class, 'getPrices']);
+Route::put('/setWeeklyPrice/{id}', [ApiController::class, 'setWeeklyPrice']);
 
 // Reservations
-Route::post('/reservation', 'ApiController@createReservation');
-Route::get('/reservation/{reservationId}', 'ApiController@getReservation');
-Route::put('/reservation/{reservationId}', 'ApiController@confirmReservation');
-Route::delete('/reservation/{reservationId}', 'ApiController@cancelReservation');
-Route::get('/reservations/{year}', 'ApiController@getReservations');
+Route::post('/reservation', [ApiController::class, 'createReservation']);
+Route::get('/reservation/{reservationId}', [ApiController::class, 'getReservation']);
+Route::put('/reservation/{reservationId}', [ApiController::class, 'confirmReservation']);
+Route::delete('/reservation/{reservationId}', [ApiController::class, 'cancelReservation']);
+Route::get('/reservations/{year}', [ApiController::class, 'getReservations']);
 
 // Availability
-Route::get('/availability/{year}', 'ApiController@getAvailability');
-Route::get('/shortAvailability/{year}', 'ApiController@getShortAvailability');
+Route::get('/availability/{year}', [ApiController::class, 'getAvailability']);
+Route::get('/shortAvailability/{year}', [ApiController::class, 'getShortAvailability']);
 
 // Documents
-Route::post('/addDocument/{itemType}', 'ApiController@addDocument');
+Route::post('/addDocument/{itemType}', [ApiController::class, 'addDocument']);
 
 // Crew List
-Route::get('/crewListLink/{reservationId}', 'ApiController@getCrewListLink');
+Route::get('/crewListLink/{reservationId}', [ApiController::class, 'getCrewListLink']);
 
 // Invoices
-Route::get('/invoices/{invoiceType}', 'ApiController@getInvoices');
+Route::get('/invoices/{invoiceType}', [ApiController::class, 'getInvoices']);
 
 // Users
-Route::get('/users', 'ApiController@getUsers');
-Route::post('/users', 'ApiController@createUser');
-Route::post('/users/search', 'ApiController@searchUsers');
-Route::get('/users/{id}', 'ApiController@getUser');
-Route::put('/users/{id}', 'ApiController@updateUser');
+Route::get('/users', [ApiController::class, 'getUsers']);
+Route::post('/users', [ApiController::class, 'createUser']);
+Route::post('/users/search', [ApiController::class, 'searchUsers']);
+Route::get('/users/{id}', [ApiController::class, 'getUser']);
+Route::put('/users/{id}', [ApiController::class, 'updateUser']);
 
 // Skippers
-Route::get('/skippers', 'ApiController@getSkippers');
+Route::get('/skippers', [ApiController::class, 'getSkippers']);
 
 // General
-Route::get('/objects/{entity}/properties', 'ApiController@getEntityProperties');
+Route::get('/objects/{entity}/properties', [ApiController::class, 'getEntityProperties']);
--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/yachts', function () {
-    $response = Http::withHeaders([
-        'Accept' => 'application/json',
-        'Authorization' => config('services.api_key_secret')
-    ])
-    ->get('https://www.booking-manager.com/api/v2/countries');
-
-    // Check if the response was successful
-    if ($response->successful()) {
-        return response()->json($response->json());
-    } else {
-        return response()->json(['error' => 'Unable to fetch countries'], 500);
-    }
-});
-
