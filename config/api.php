ad8f2f6aad5a57c61e568817171a8ea903a0641d
JanSztefek <sztefek.jan@seznam.cz>
2025-03-17 13:00:59 +0100

    rm all

--- config/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return[
-    'base_url' => match (env('API_ENV', 'production')) {
-        'beta' => 'http://beta.booking-manager.com/api/v2',
-        'swagger' => 'https://virtserver.swaggerhub.com/mmksystems/bm-api/2.0.0',
-        default => 'https://www.booking-manager.com/api/v2',
-    },
-];
--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Http;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
